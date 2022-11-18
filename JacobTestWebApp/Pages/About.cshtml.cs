ec56b6a3cbb9c3784d6f6c2d54157784fe749352
Jacob <jsmith@synergytaste.com>
2022-11-18 12:27:49 +0000

    Sort filter page and group complete.

---
+++ JacobTestWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,37 @@
+using JacobTestWebApp.Models.SchoolViewModels;
+using JacobTestWebApp.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JacobTestWebApp.Models;
+
+namespace JacobTestWebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AboutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
