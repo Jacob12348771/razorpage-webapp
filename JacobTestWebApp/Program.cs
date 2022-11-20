0b2da16f85e29bd30f81b2ab63baecc4fea9a674
Jacob <jsmith@synergytaste.com>
2022-11-20 17:43:52 +0000

    Steps 4 and 5 complete.

--- JacobTestWebApp/Program.cs
+++ JacobTestWebApp/Program.cs
@@ -37,7 +37,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.EnsureCreated();
+    // Ensure created removed.
     DbInitializer.Initialize(context);
 }
 
